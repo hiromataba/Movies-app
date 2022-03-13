24f76fd445a66e997cc609408ca696b515a3bccb
Hiro Mataba <aganzemataba@gmail.com>
2022-03-13 17:25:02 +0200

    Add reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,63 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def edit
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to review_url(@review), notice: "Review was successfully created." }
+        format.json { render :show, status: :created, location: @review }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @review.update(review_params)
+        format.html { redirect_to review_url(@review), notice: "Review was successfully updated." }
+        format.json { render :show, status: :ok, location: @review }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @review.destroy
+
+    respond_to do |format|
+      format.html { redirect_to reviews_url, notice: "Review was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_review
+      @review = Review.find(params[:id])
+    end
+
+    def review_params
+      params.require(:review).permit(:rating, :comment)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_13_073801) do
+ActiveRecord::Schema.define(version: 2022_03_13_150948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2022_03_13_073801) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.string "image"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
