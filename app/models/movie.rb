82031f8e9d0ff4ad563f7b33a38e1cd1481d97f6
Hiro Mataba <aganzemataba@gmail.com>
2022-03-16 11:08:48 +0200

    Add a bit of styloe to the app

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,5 @@
 class Movie < ApplicationRecord
 	belongs_to :user
 	has_many :reviwes
-	# has_attached_file :image, styles: { medium: "400x600#" }
-  # validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	mount_uploader :image, ImageUploader
 end
